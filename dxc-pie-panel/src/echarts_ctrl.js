44f32d6edd113ca8937c065e87fb353f3f42ee4a
Wang <qianxiang.wang@hpe.com>
2017-07-17 14:10:33 +0800

    增加饼图

---
+++ dxc-pie-panel/src/echarts_ctrl.js
@@ -0,0 +1,152 @@
+import { MetricsPanelCtrl } from 'app/plugins/sdk';
+import _ from 'lodash';
+import echarts from './libs/echarts.min';
+import './libs/echarts-liquidfill.min';
+import './libs/dark';
+import './style.css!';
+
+export class EchartsCtrl extends MetricsPanelCtrl {
+
+    constructor($scope, $injector) {
+        super($scope, $injector);
+
+        const panelDefaults = {
+            EchartsOption: 'console.log(JSON.stringify(echartsData));\n\n option = {};',
+            USE_URL: false,
+            USE_FAKE_DATA: true,
+            fakeData: '',
+            url: '',
+            request: '',
+            updateInterval: 10000
+        };
+
+        _.defaults(this.panel, panelDefaults);
+
+        this.events.on('data-received', this.onDataReceived.bind(this));
+        this.events.on('data-error', this.onDataError.bind(this));
+        this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
+        this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+        this.events.on('panel-initialized', this.render.bind(this));
+
+        this.updateData();
+    }
+
+    //post请求
+    updateData() {
+        let that = this, xmlhttp;
+
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                that.UrlData = JSON.parse(xmlhttp.responseText);
+                that.onDataReceived();
+            }
+        };
+
+        if (that.panel.USE_URL && !that.panel.USE_FAKE_DATA && that.panel.url && that.panel.request) {
+            xmlhttp.open("POST", that.panel.url, true);
+            xmlhttp.send(that.panel.request);
+        } else {
+            xmlhttp = null;
+        }
+
+        this.$timeout(() => { this.updateData(); }, that.panel.updateInterval);
+    }
+
+    onDataReceived(dataList) {
+        this.data = this.panel.USE_URL ? this.UrlData : dataList;
+
+        if (this.panel.USE_URL && this.panel.USE_FAKE_DATA && this.panel.fakeData) {
+            this.data = eval(this.panel.fakeData); // jshint ignore:line
+        }
+
+        this.IS_DATA_CHANGED = true;
+        this.render();
+        this.IS_DATA_CHANGED = false;
+    }
+
+    onDataError(err) {
+        this.render();
+    }
+
+    onInitEditMode() {
+        this.addEditorTab('数据', 'public/plugins/dxc-echarts-panel/editer-metric.html', 2);
+        this.addEditorTab('Ecahrts配置', 'public/plugins/dxc-echarts-panel/editor-echarts.html', 3);
+    }
+
+    link(scope, elem, attrs, ctrl) {
+        const $panelContainer = elem.find('.echarts_container')[0];
+        let option = {},
+            echartsData = [];
+
+        ctrl.IS_DATA_CHANGED = true;
+
+        function setHeight() {
+            let height = ctrl.height || panel.height || ctrl.row.height;
+            if (_.isString(height)) {
+                height = parseInt(height.replace('px', ''), 10);
+            }
+            $panelContainer.style.height = height + 'px';
+        }
+
+        setHeight();
+
+        let myChart = echarts.init($panelContainer, 'dark');
+
+        // bad hank
+        setTimeout(function () {
+            myChart.resize();
+        }, 1000);
+
+        // 防止重复触发事件
+        var callInterval = function callInterval() {
+            var timeout, result;
+
+            function func(callBack, interval) {
+                var context = this; // jshint ignore:line
+                var args = arguments;
+
+                if (timeout) clearInterval(timeout);
+
+                timeout = setInterval(function () {
+                    result = callBack.apply(context, args);
+                }, interval);
+
+                return result;
+            }
+
+            return func;
+        }();
+
+        function render() {
+
+            if (!myChart) {
+                return;
+            }
+
+            setHeight();
+            myChart.resize();
+
+            if (ctrl.IS_DATA_CHANGED) {
+                myChart.clear();
+                echartsData = ctrl.data;
+
+                eval(ctrl.panel.EchartsOption); // jshint ignore:line
+
+                myChart.setOption(option);
+            }
+        }
+
+        this.events.on('render', function () {
+            render();
+            ctrl.renderingCompleted();
+        });
+    }
+}
+
+EchartsCtrl.templateUrl = 'module.html';
