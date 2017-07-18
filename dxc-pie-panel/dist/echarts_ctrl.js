db8f130c3a219087a77dc0fbce3e736ba3817c62
Wang <qianxiang.wang@hpe.com>
2017-07-18 18:10:07 +0800

    增强饼图

--- dxc-pie-panel/dist/echarts_ctrl.js
+++ dxc-pie-panel/dist/echarts_ctrl.js
@@ -71,16 +71,33 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                     var _this = _possibleConstructorReturn(this, (EchartsCtrl.__proto__ || Object.getPrototypeOf(EchartsCtrl)).call(this, $scope, $injector));
 
                     var panelDefaults = {
-                        EchartsOption: 'console.log(JSON.stringify(echartsData));\n\n option = {};',
+                        echartsOption: {
+                            colorArr: ['#85b6b2', '#6d4f8d', '#cd5e7e', '#e38980', '#f7db88'],
+                            title: '主机容量',
+                            subTitle: '',
+                            titleX: 'center',
+                            titleY: '',
+                            toolBoxShow: true,
+                            legendShow: true,
+                            legendOrient: 'vertical',
+                            legendTop: '',
+                            legendLeft: 'left',
+                            IS_CONCENTRIC: true,
+                            roseType: 'area',
+                            minRadius: '0%',
+                            maxRadius: '75%',
+                            centerX: '50%',
+                            centerY: '50%'
+                        },
                         USE_URL: false,
                         USE_FAKE_DATA: true,
-                        fakeData: '',
+                        fakeData: '[{"target":"已用容量","datapoints":[["115",1499285716736]]},{"target":"剩余容量","datapoints":[["87",1499285716736]]}]',
                         url: '',
                         request: '',
                         updateInterval: 10000
                     };
 
-                    _.defaults(_this.panel, panelDefaults);
+                    _.defaultsDeep(_this.panel, panelDefaults);
 
                     _this.events.on('data-received', _this.onDataReceived.bind(_this));
                     _this.events.on('data-error', _this.onDataError.bind(_this));
@@ -88,7 +105,14 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                     _this.events.on('init-edit-mode', _this.onInitEditMode.bind(_this));
                     _this.events.on('panel-initialized', _this.render.bind(_this));
 
-                    _this.updateData();
+                    if (_this.panel.USE_URL && _this.panel.USE_FAKE_DATA && _this.panel.fakeData) {
+                        var that = _this;
+                        setTimeout(function () {
+                            that.onDataReceived(that.panel.fakeData);
+                        }, 500);
+                    } else {
+                        _this.updateData();
+                    }
                     return _this;
                 }
 
@@ -136,9 +160,31 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                             this.data = eval(this.panel.fakeData); // jshint ignore:line
                         }
 
+                        this.data = this.translateData(this.data);
+                        this.onRender();
+                    }
+                }, {
+                    key: 'onRender',
+                    value: function onRender() {
                         this.IS_DATA_CHANGED = true;
                         this.render();
                         this.IS_DATA_CHANGED = false;
+                    }
+                }, {
+                    key: 'translateData',
+                    value: function translateData(data) {
+                        if (data.length) {
+                            var dataArr = [];
+                            for (var i = 0; i < data.length; i++) {
+                                dataArr.push({
+                                    name: data[i].target,
+                                    value: data[i].datapoints[data[i].datapoints.length - 1][0]
+                                });
+                            }
+                            return dataArr;
+                        } else {
+                            return [];
+                        }
                     }
                 }, {
                     key: 'onDataError',
@@ -148,15 +194,32 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                 }, {
                     key: 'onInitEditMode',
                     value: function onInitEditMode() {
+                        this.subTabIndex = 0;
                         this.addEditorTab('数据', 'public/plugins/dxc-pie-panel/editer-metric.html', 2);
                         this.addEditorTab('Echarts配置', 'public/plugins/dxc-pie-panel/editor-echarts.html', 3);
+                    }
+                }, {
+                    key: 'changeColor',
+                    value: function changeColor(colorIndex, color) {
+                        this.panel.echartsOption.colorArr[colorIndex] = color;
+                        this.onRender();
+                    }
+                }, {
+                    key: 'removeColor',
+                    value: function removeColor(colorIndex) {
+                        this.panel.echartsOption.colorArr.splice(colorIndex, 1);
+                        this.onRender();
+                    }
+                }, {
+                    key: 'addColor',
+                    value: function addColor() {
+                        this.panel.echartsOption.colorArr.push('rgba(255, 255, 255, 1)');
+                        this.onRender();
                     }
                 }, {
                     key: 'link',
                     value: function link(scope, elem, attrs, ctrl) {
                         var $panelContainer = elem.find('.echarts_container')[0];
-                        var option = {},
-                            echartsData = [];
 
                         ctrl.IS_DATA_CHANGED = true;
 
@@ -171,6 +234,7 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                         setHeight();
 
                         var myChart = echarts.init($panelContainer, 'dark');
+                        myChart.resize();
 
                         // 防止重复触发事件
                         var callInterval = function callInterval() {
@@ -193,22 +257,127 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                         }();
 
                         function render() {
-
-                            if (!myChart) {
-                                return;
-                            }
+                            if (!myChart) return;
 
                             setHeight();
                             myChart.resize();
 
                             if (ctrl.IS_DATA_CHANGED) {
                                 myChart.clear();
-                                echartsData = ctrl.data;
 
-                                eval(ctrl.panel.EchartsOption); // jshint ignore:line
-
-                                myChart.setOption(option);
+                                myChart.setOption({
+                                    backgroundColor: '#1f1d1d',
+                                    color: ctrl.panel.echartsOption.colorArr,
+                                    title: {
+                                        text: ctrl.panel.echartsOption.title,
+                                        subtext: ctrl.panel.echartsOption.subTitle,
+                                        x: ctrl.panel.echartsOption.titleX,
+                                        y: ctrl.panel.echartsOption.titleY,
+                                        textStyle: {
+                                            // fontWeight: 'normal'
+                                        }
+                                    },
+                                    toolbox: {
+                                        show: ctrl.panel.echartsOption.toolBoxShow,
+                                        feature: {
+                                            dataView: {
+                                                show: true,
+                                                readOnly: false
+                                            },
+                                            saveAsImage: {
+                                                show: true
+                                            }
+                                        }
+                                    },
+                                    tooltip: {
+                                        trigger: 'item',
+                                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                                    },
+                                    legend: {
+                                        show: ctrl.panel.echartsOption.legendShow,
+                                        orient: ctrl.panel.echartsOption.legendOrient,
+                                        top: ctrl.panel.echartsOption.legendTop,
+                                        left: ctrl.panel.echartsOption.legendLeft,
+                                        data: getLegend()
+                                    },
+                                    series: getSeries()
+                                });
                             }
+                        }
+
+                        function getLegend() {
+                            if (ctrl.data) {
+                                var legend = [];
+                                for (var i = 0; i < ctrl.data.length; i++) {
+                                    legend.push(ctrl.data[i].name);
+                                }
+                                return legend;
+                            }
+                        }
+
+                        function getSeries() {
+                            var seriesArr = [];
+
+                            if (!ctrl.panel.echartsOption.IS_CONCENTRIC) {
+                                seriesArr.push({
+                                    name: ctrl.panel.echartsOption.title,
+                                    type: 'pie',
+                                    radius: [ctrl.panel.echartsOption.minRadius, ctrl.panel.echartsOption.maxRadius],
+                                    center: [ctrl.panel.echartsOption.centerX, ctrl.panel.echartsOption.centerY],
+                                    roseType: ctrl.panel.echartsOption.roseType,
+                                    data: ctrl.data,
+                                    itemStyle: {
+                                        emphasis: {
+                                            shadowBlur: 10,
+                                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                        }
+                                    }
+                                });
+                            } else {
+                                if (ctrl.data) {
+                                    for (var i = 0; i < Math.floor(ctrl.data.length / 2); i++) {
+                                        seriesArr.push({
+                                            name: ctrl.panel.echartsOption.title,
+                                            type: 'pie',
+                                            clockWise: false,
+                                            hoverAnimation: false,
+                                            radius: getRadius(i, ctrl.panel.echartsOption.minRadius, ctrl.panel.echartsOption.maxRadius),
+                                            center: [ctrl.panel.echartsOption.centerX, ctrl.panel.echartsOption.centerY],
+                                            data: [ctrl.data[2 * i], {
+                                                name: '',
+                                                value: ctrl.data[2 * i + 1].value,
+                                                itemStyle: {
+                                                    normal: {
+                                                        color: 'rgba(0,0,0,0)',
+                                                        label: { show: false },
+                                                        labelLine: { show: false }
+                                                    },
+                                                    emphasis: {
+                                                        color: 'rgba(0,0,0,0)'
+                                                    }
+                                                }
+                                            }],
+                                            itemStyle: {
+                                                emphasis: {
+                                                    shadowBlur: 10,
+                                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                                }
+                                            }
+                                        });
+                                    }
+                                } else {
+                                    seriesArr = [];
+                                }
+                            }
+
+                            return seriesArr;
+                        }
+
+                        function getRadius(index, min, max) {
+                            var reg = /%+/;
+                            var unit = reg.test(min) && reg.test(max) ? '%' : 0;
+                            var interval = (parseFloat(max) - parseFloat(min)) / ctrl.data.length;
+                            return [interval * index + parseFloat(min) + unit, interval * (index + 1) + parseFloat(min) + unit];
                         }
 
                         this.events.on('render', function () {
--- dxc-pie-panel/src/echarts_ctrl.js
+++ dxc-pie-panel/src/echarts_ctrl.js
@@ -11,16 +11,33 @@ export class EchartsCtrl extends MetricsPanelCtrl {
         super($scope, $injector);
 
         const panelDefaults = {
-            EchartsOption: 'console.log(JSON.stringify(echartsData));\n\n option = {};',
+            echartsOption: {
+                colorArr: ['#85b6b2', '#6d4f8d', '#cd5e7e', '#e38980', '#f7db88'],
+                title: '主机容量',
+                subTitle: '',
+                titleX: 'center',
+                titleY: '',
+                toolBoxShow: true,
+                legendShow: true,
+                legendOrient: 'vertical',
+                legendTop: '',
+                legendLeft: 'left',
+                IS_CONCENTRIC: true,
+                roseType: 'area',
+                minRadius: '0%',
+                maxRadius: '75%',
+                centerX: '50%',
+                centerY: '50%'
+            },
             USE_URL: false,
             USE_FAKE_DATA: true,
-            fakeData: '',
+            fakeData: '[{"target":"已用容量","datapoints":[["115",1499285716736]]},{"target":"剩余容量","datapoints":[["87",1499285716736]]}]',
             url: '',
             request: '',
             updateInterval: 10000
         };
 
-        _.defaults(this.panel, panelDefaults);
+        _.defaultsDeep(this.panel, panelDefaults);
 
         this.events.on('data-received', this.onDataReceived.bind(this));
         this.events.on('data-error', this.onDataError.bind(this));
@@ -28,7 +45,14 @@ export class EchartsCtrl extends MetricsPanelCtrl {
         this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
         this.events.on('panel-initialized', this.render.bind(this));
 
-        this.updateData();
+        if (this.panel.USE_URL && this.panel.USE_FAKE_DATA && this.panel.fakeData) {
+            let that = this;
+            setTimeout(function () {
+                that.onDataReceived(that.panel.fakeData);
+            }, 500);
+        } else {
+            this.updateData();
+        }
     }
 
     //post请求
@@ -65,9 +89,29 @@ export class EchartsCtrl extends MetricsPanelCtrl {
             this.data = eval(this.panel.fakeData); // jshint ignore:line
         }
 
+        this.data = this.translateData(this.data);
+        this.onRender();
+    }
+
+    onRender() {
         this.IS_DATA_CHANGED = true;
         this.render();
         this.IS_DATA_CHANGED = false;
+    }
+
+    translateData(data) {
+        if (data.length) {
+            let dataArr = [];
+            for (let i = 0; i < data.length; i++) {
+                dataArr.push({
+                    name: data[i].target,
+                    value: data[i].datapoints[data[i].datapoints.length - 1][0]
+                });
+            }
+            return dataArr;
+        } else {
+            return [];
+        }
     }
 
     onDataError(err) {
@@ -75,14 +119,33 @@ export class EchartsCtrl extends MetricsPanelCtrl {
     }
 
     onInitEditMode() {
+        this.subTabIndex = 0;
         this.addEditorTab('数据', 'public/plugins/dxc-pie-panel/editer-metric.html', 2);
         this.addEditorTab('Echarts配置', 'public/plugins/dxc-pie-panel/editor-echarts.html', 3);
     }
 
+    changeColor(colorIndex, color) {
+        this.panel.echartsOption.colorArr[colorIndex] = color;
+        this.onRender();
+    }
+
+    removeColor(colorIndex) {
+        this.panel.echartsOption.colorArr.splice(colorIndex, 1);
+        this.onRender();
+    }
+
+    addColor() {
+        this.panel.echartsOption.colorArr.push('rgba(255, 255, 255, 1)');
+        this.onRender();
+    }
+
+    // invertColor() {
+    //     this.panel.echartsOption.colorArr.reverse();
+    //     this.onRender();
+    // }
+
     link(scope, elem, attrs, ctrl) {
         const $panelContainer = elem.find('.echarts_container')[0];
-        let option = {},
-            echartsData = [];
 
         ctrl.IS_DATA_CHANGED = true;
 
@@ -97,6 +160,7 @@ export class EchartsCtrl extends MetricsPanelCtrl {
         setHeight();
 
         let myChart = echarts.init($panelContainer, 'dark');
+        myChart.resize();
 
         // 防止重复触发事件
         var callInterval = function callInterval() {
@@ -119,22 +183,130 @@ export class EchartsCtrl extends MetricsPanelCtrl {
         }();
 
         function render() {
-
-            if (!myChart) {
-                return;
-            }
+            if (!myChart) return;
 
             setHeight();
             myChart.resize();
 
             if (ctrl.IS_DATA_CHANGED) {
                 myChart.clear();
-                echartsData = ctrl.data;
 
-                eval(ctrl.panel.EchartsOption); // jshint ignore:line
-
-                myChart.setOption(option);
+                myChart.setOption({
+                    backgroundColor: '#1f1d1d',
+                    color: ctrl.panel.echartsOption.colorArr,
+                    title: {
+                        text: ctrl.panel.echartsOption.title,
+                        subtext: ctrl.panel.echartsOption.subTitle,
+                        x: ctrl.panel.echartsOption.titleX,
+                        y: ctrl.panel.echartsOption.titleY,
+                        textStyle: {
+                            // fontWeight: 'normal'
+                        }
+                    },
+                    toolbox: {
+                        show: ctrl.panel.echartsOption.toolBoxShow,
+                        feature: {
+                            dataView: {
+                                show: true,
+                                readOnly: false
+                            },
+                            saveAsImage: {
+                                show: true
+                            }
+                        }
+                    },
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    },
+                    legend: {
+                        show: ctrl.panel.echartsOption.legendShow,
+                        orient: ctrl.panel.echartsOption.legendOrient,
+                        top: ctrl.panel.echartsOption.legendTop,
+                        left: ctrl.panel.echartsOption.legendLeft,
+                        data: getLegend()
+                    },
+                    series: getSeries()
+                });
             }
+        }
+
+        function getLegend() {
+            if (ctrl.data) {
+                let legend = [];
+                for (let i = 0; i < ctrl.data.length; i++) {
+                    legend.push(ctrl.data[i].name);
+                }
+                return legend;
+            }
+        }
+
+        function getSeries() {
+            let seriesArr = [];
+
+            if (!ctrl.panel.echartsOption.IS_CONCENTRIC) {
+                seriesArr.push({
+                    name: ctrl.panel.echartsOption.title,
+                    type: 'pie',
+                    radius: [ctrl.panel.echartsOption.minRadius, ctrl.panel.echartsOption.maxRadius],
+                    center: [ctrl.panel.echartsOption.centerX, ctrl.panel.echartsOption.centerY],
+                    roseType: ctrl.panel.echartsOption.roseType,
+                    data: ctrl.data,
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                });
+            } else {
+                if (ctrl.data) {
+                    for (let i = 0; i < Math.floor(ctrl.data.length / 2); i++) {
+                        seriesArr.push({
+                            name: ctrl.panel.echartsOption.title,
+                            type: 'pie',
+                            clockWise: false,
+                            hoverAnimation: false,
+                            radius: getRadius(i, ctrl.panel.echartsOption.minRadius, ctrl.panel.echartsOption.maxRadius),
+                            center: [ctrl.panel.echartsOption.centerX, ctrl.panel.echartsOption.centerY],
+                            data: [
+                                ctrl.data[2 * i],
+                                {
+                                    name: '',
+                                    value: ctrl.data[2 * i + 1].value,
+                                    itemStyle: {
+                                        normal: {
+                                            color: 'rgba(0,0,0,0)',
+                                            label: { show: false },
+                                            labelLine: { show: false }
+                                        },
+                                        emphasis: {
+                                            color: 'rgba(0,0,0,0)'
+                                        }
+                                    }
+                                }
+                            ],
+                            itemStyle: {
+                                emphasis: {
+                                    shadowBlur: 10,
+                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    seriesArr = [];
+                }
+            }
+
+            return seriesArr;
+        }
+
+        function getRadius(index, min, max) {
+            const reg = /%+/;
+            let unit = reg.test(min) && reg.test(max) ? '%' : 0;
+            let interval = (parseFloat(max) - parseFloat(min)) / ctrl.data.length;
+            return [(interval * index + parseFloat(min) + unit), (interval * (index + 1) + parseFloat(min) + unit)];
         }
 
         this.events.on('render', function () {
